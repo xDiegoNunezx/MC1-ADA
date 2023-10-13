5e25769198c0c84fdb9ecba57dbfb9ada7d31593
Diego Ignacio Núñez Hernández <diego.nunezh@ingenieria.unam.edu>
2023-10-13 17:39:01 +0200

    Project Created

---
+++ MC1-ADA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MC1-ADA
+//
+//  Created by Diego Ignacio Nunez Hernandez on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MC1-ADA/MC1_ADAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC1_ADAApp.swift
+//  MC1-ADA
+//
+//  Created by Diego Ignacio Nunez Hernandez on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MC1_ADAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
