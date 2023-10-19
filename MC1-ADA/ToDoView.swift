63014d8244d0fa867f4879dcd8a6943c1b1e52ec
Alejandro Oliva Ochoa <alejandroolivaochoa@10-20-60-8.0200.fed.it.iosda.org>
2023-10-19 15:59:48 +0200

    ToDoView created and color scheme added

---
+++ MC1-ADA/ToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoView.swift
+//  MC1-ADA
+//
+//  Created by Alejandro Oliva Ochoa on 19/10/23.
+//
+
+import SwiftUI
+
+struct ToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoView()
+}
