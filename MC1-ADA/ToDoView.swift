adf37afc8750cb64cd590b3217a90a58de5721c2
Alejandro Oliva <alex_oliva1000@hotmail.com>
2023-10-19 16:07:42 +0200

    Added the initial message to the ToDoView

--- MC1-ADA/ToDoView.swift
+++ MC1-ADA/ToDoView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ToDoView: View {
+    
+    var toDoList: [String] = [
+        "Hola"
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            
+            ZStack{
+                Color(red: 0.94, green: 0.94, blue: 0.94)
+                    .frame(height: 150)
+                    .cornerRadius(15)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .shadow(color: .black.opacity(0.1), radius: 2, x: 1, y: 1)
+                    
+                
+                Text("Write your assignments in the order of importance you think they have")
+                    .font(.system(size: 25, weight: .bold))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.greenTheme)
+                    .bold()
+                    .padding(.horizontal, 15.5)
+            }
+            .padding()
+            .navigationTitle("Sort It Out")
+            
+        }
     }
 }
 
 #Preview {
     ToDoView()
 }
+
