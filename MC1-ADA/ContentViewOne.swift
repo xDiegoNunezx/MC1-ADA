5ef07a32a488bfb42dea77d38379be795cbeb498
EConti23 <mvpconti@gmail.com>
2023-10-24 16:18:09 +0200

    breath finished

--- MC1-ADA/ContentViewOne.swift
+++ MC1-ADA/ContentViewOne.swift
@@ -7,56 +7,63 @@
 import SwiftUI
 
 struct ContentViewOne: View {
-   @State var countDownTimer = 3
-   @State var timerRunning = true
-   let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-   @State var active = false
-   
+    
+    @State var countDownTimer = 5
+    @State var timerRunning = true
+    @State var timer: Timer.TimerPublisher = Timer.publish(every: 1, on: .main, in: .common)
+    @State var active = false
+    @State var inhaleActive = true
+    @State var timeLabel = "Press to inhale"
+    
+    func starTimer(){
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+        _ = timer.connect()
+    }
+    
+    func stopTimer(){
+        timer.connect().cancel()
+    }
+    
     var body: some View {
-        
-        NavigationView {
-                    VStack {
-                        NavigationStack {
-                            Button{
-                                timerRunning = true
-                                countDownTimer = 3
-                                print("pressed")
-                            } label: {
-                                Image("Inhale")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width:330)
-                            }
-                            Image(systemName: "timer")
-                            Text("\(countDownTimer)")
-                                .onReceive(timer) { _ in
-                                    if countDownTimer > 0 && timerRunning {
-                                        countDownTimer -= 1
-                                    } else {
-                                        timerRunning = false
-                                    }
-                                }
-                            Text("Hold to inhale")
-                                .bold()
-                                .font(.largeTitle)
-                            Text("Release to exhale")
-                                .bold()
-                                .navigationBarTitle("Breath Exercises")
+        NavigationStack {
+            VStack {
+                Button{
+                    starTimer()
+                } label: {
+                    Image("Exhale")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:330)
+                }
+                Image(systemName: "timer")
+                
+                Text("\(countDownTimer)")
+                    .onReceive(timer) { _ in
+                        if countDownTimer > 0 {
+                            countDownTimer -= 1
+                        } else {
                             
-                                .navigationBarItems(leading: HStack {
-                                    Image(systemName: "chevron.backward")
-                                    NavigationLink("Back", destination: Text("MainPage")
-                                        .navigationBarBackButtonHidden(true))
-                                    .foregroundColor(.black)
-                                },
-                                                    trailing: Image(systemName: "figure.mind.and.body"))
+                            if(inhaleActive){
+                                timeLabel = "Exhale"
+                            } else {
+                                timeLabel = "Inhale"
+                            }
+                            inhaleActive = !inhaleActive
+                                                                        
+                            stopTimer()
+                            countDownTimer = 5
+                            starTimer()
                         }
                     }
-                    .padding()
+                Text(timeLabel)
+                    .bold()
+                    .font(.largeTitle)
+                    .navigationTitle("Breath Exercises")
+            }
         }
         .padding()
     }
 }
-            #Preview {
-                    ContentViewOne()
-                }
+#Preview {
+    ContentViewOne()
+}
