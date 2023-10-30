c592aef2f50e90f43d8b28bdff0a9fb90fa77363
KaranO-Dev <119845259+KaranO-Dev@users.noreply.github.com>
2023-10-30 21:22:17 +0100

    Version 0.1 of app with Onboarding screen is now live!
    Added Onboarding screen with the illustration that i made,
    
    Changed the app name, version and the category,
    
    I tried to implement the scroll bar at the bottom but it somehow breaks it,
    
    tried to make every page with images and the texts in the same position but it didn't work,
    
    let me know if you wanna fix it

--- MC1-ADA/OnboardingView.swift
+++ MC1-ADA/OnboardingView.swift
@@ -7,65 +7,125 @@
 
 import SwiftUI
 
+
 struct OnboardingView: View {
+    @AppStorage("isFirstLaunch") private var isFirstLaunch: Bool = true
+    private var previewIsFirstLaunch: Bool = true
+    
+    
     var body: some View {
-        TabView {
-            VStack {
-                Image("onboarding-image")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height:250)
+        if isFirstLaunch  {
+            TabView {
+                VStack {
+                    Image("onboarding-image1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 400, alignment: .center)
+                    
+                    Text("Welcome to iMind")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.greenTheme)
+                        .frame(width: 300, height: 50, alignment: .center)
+                    
+                    
+                }
+                .tag(0)
                 
-                Text("Welcome to iMind")
-                    .font(.title)
-                    .bold()
-                    .tabItem {
-                        Image(systemName: "1.circle")
-                        Text("Page 1")
+                VStack {
+                    Image("onboarding-image2")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300, alignment: .center)
+
+                    VStack {
+                        Text("What is iMind?")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.greenTheme)
+                            .frame(width: 300, height: 50, alignment: .center)
+                        
+                        Text("iMind is an app focused on guiding individuals in achieving mindfulness and relaxation through simple instructions.")
+                            .multilineTextAlignment(.center)
+                            .font(Font.custom("SF Pro Rounded", size: 20))
+                            .frame(width: 300, height: 150, alignment: .center)
                     }
-            }
-            VStack {
-                Image("onboarding-image2")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height:250)
-                Text("What is iMind?")
-                    .font(.title)
-                    .bold()
-                Text("iMind is an app focused on guiding individuals in achieving mindfulness and relaxation through simple instructions.")
-                    .multilineTextAlignment(.center)
-                    .font(Font.custom("SF Pro", size: 20))
-                    .tabItem {
+                }
+                .tag(1)
+
+                VStack {
+                    Image("onboarding-image3")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300, alignment: .center)
+                        .padding()
+
+                    VStack {
+                        Text("What is it for?")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.greenTheme)
+                            .frame(width: 300, height: 50, alignment: .center)
+
+                        Text("Can serve as a vital tool to help individuals alleviate stress, calm their minds, and regain a sense of balance.")
+                            .font(Font.custom("SF Pro Rounded", size: 20))
+                            .multilineTextAlignment(.center)
+                            .frame(width: 300, height: 150, alignment: .center)
                     }
-                    .padding()
-            }
-            VStack {
-                Image("onboarding-image3")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height:250)
-                Text("What is it for?")
-                    .font(.title)
-                    .bold()
-                Text("Can serve as a vital tool to help individuals alleviate stress, calm their minds, and regain a sense of balance.")
-                    .font(Font.custom("SF Pro", size: 20))
-                    .multilineTextAlignment(.center)
-                    .tabItem {
+                }
+                .tag(2)
+
+                VStack {
+                    Image("onboarding-image4")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300, alignment: .center)
+                        .padding()
+
+                    VStack {
+                        Text("Why wait then?")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.greenTheme)
+                            .frame(width: 300, height: 50, alignment: .center)
+
+                        Text("Let's get started! Start with meditation, try the breathing exercises and don't forget to check-in daily!")
+                            .font(Font.custom("SF Pro Rounded", size: 20))
+                            .multilineTextAlignment(.center)
+                            .frame(width: 300, height: 150, alignment: .center)
                     }
-                    .padding()
+                }
+                .tag(3)
+
+                
             }
+            .tabViewStyle(PageTabViewStyle())
+            
+            .overlay(
+                Button(action: {
+                    isFirstLaunch = false
+                }) {
+                    Text("Skip")
+                        .bold()
+                        .padding()
+                        .background(Color.greenTheme)
+                        .foregroundColor(.white)
+                        .cornerRadius(25)
+                }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                , alignment: Alignment.bottomTrailing
+            )
+            .padding()
+        } else {
+            MainMenuView() // Your main view after onboarding
         }
-        .tabViewStyle(PageTabViewStyle())
     }
 }
 
-struct YourApp: App {
-    var body: some Scene {
-        WindowGroup {
-            OnboardingView()
-        }
+
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
     }
-}
-#Preview {
-    OnboardingView()
 }
